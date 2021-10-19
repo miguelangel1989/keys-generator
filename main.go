325c6e8ce370c425aa0a7199209825e1df0021bd
miguelangel1989 <92815421+miguelangel1989@users.noreply.github.com>
2021-10-19 13:00:33 -0600

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	coin := os.Args[1]
 
-	keysPerPage := 128
+	keysPerPage := 10000000000000000000000000000000000000000000000000000000000000000000000000
 
 	switch coin {
 	case "btc":
